125602c0310cedbbc5c126c5284228806509cb17
nknouf <nknouf@1dcb0dde-224a-4fdb-a13f-176edaf759ab>
2009-03-19 22:58:22 +0000

    -- We now have RSS2 feeds!  Yay!

--- MAICgregatorServer/server.py
+++ MAICgregatorServer/server.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 import urllib
+import datetime
 
 import feedparser
+import PyRSS2Gen
+from BeautifulSoup import BeautifulSoup
 
 import post
 import web
@@ -35,6 +38,7 @@ urls = (
     '/MAICgregator/TrusteeRelationshipSearch/(.*?)', 'TrusteeSearch',
     '/MAICgregator/GoogleNews/(.*?)', 'GoogleNews',
     '/MAICgregator/Aggregate/(.*?)/(.*?)', 'Aggregate',
+    '/MAICgregator/feed/rss/(.*?)/(.*?)', 'RSS',
     '/MAICgregator/name/(.*?)', 'name'
 )
 """
@@ -90,6 +94,35 @@ class ProcessBase(object):
         #schoolData.close()
         return results
 
+    def GoogleNewsSearchRSS2(self, hostname):
+        whoisStore = self.getWhois()
+        schoolName = whoisStore.getSchoolName(hostname)
+        schoolData = self.getSchoolData(schoolName)
+
+        print "|| MAICgregator server || Getting Google News RSS"
+        results = schoolData.getGoogleNews()
+        soup = BeautifulSoup(results)
+
+        tables = soup.findAll("table")
+        items = []
+        for table in tables:
+            timestamp = schoolData.schoolMetadata['PRNews']['timestamp']
+            print table.a.contents
+            title = "".join(str(item) for item in table.a.contents)
+            description = unicode(table.findAll("font")[3])
+            url = table.a['href']
+
+            item = PyRSS2Gen.RSSItem(title = title,
+                    link = url,
+                    description = description,
+                    guid = PyRSS2Gen.Guid(url),
+                    categories = ["Google News"],
+                    author = schoolName,
+                    pubDate = datetime.datetime.fromtimestamp(timestamp))
+            items.append(item)
+
+        return items
+
     def TrusteeRelationshipSearch(self, hostname):
         #whoisStore = whois.WhoisStore()
         #schoolName = whoisStore.getSchoolName(hostname)
@@ -103,6 +136,31 @@ class ProcessBase(object):
         print "|| MAICgregator server || Getting Trustee data"
         results = schoolData.getTrustees()
         
+        return results
+
+    def TrusteeRelationshipSearchRSS2(self, hostname):
+        whoisStore = self.getWhois()
+        schoolName = whoisStore.getSchoolName(hostname)
+        schoolData = self.getSchoolData(schoolName)
+
+        print "|| MAICgregator server || Getting Trustee RSS data"
+        results = schoolData.getTrustees()
+
+        resultList = results.split("\n")
+        items = []
+        for result in resultList:
+            timestamp = schoolData.schoolMetadata['Trustees']['timestamp']
+            url = "http://www.google.com/search?&q=" + urllib.quote(result + " trustee")
+            item = PyRSS2Gen.RSSItem(title = result,
+                    link = url,
+                    description = result,
+                    guid = PyRSS2Gen.Guid(url),
+                    categories = ["Trustee"],
+                    pubDate = datetime.datetime.fromtimestamp(timestamp))
+            items.append(item)
+
+        return items
+       
         return results
 
     def DoDBR(self, hostname):
@@ -122,6 +180,41 @@ class ProcessBase(object):
         
         return results
 
+    def DoDBRRSS2(self, hostname):
+        #whoisStore = whois.WhoisStore()
+        #schoolName = whoisStore.getSchoolName(hostname)
+        #schoolData = db.SchoolData(schoolName)
+
+        whoisStore = self.getWhois()
+        schoolName = whoisStore.getSchoolName(hostname)
+        schoolData = self.getSchoolData(schoolName)
+
+        # TODO
+        # Make this less atomic; allow the ability to return smaller chunks, random bits, etc.
+        # This means we need to come up with a REST api, as well as return error messages
+        print "|| MAICgregator server || Getting DoDBR RSS data"
+        results = schoolData.getXML()
+        
+        resultList = results.split("\n")
+        items = []
+        for result in resultList:
+            timestamp = schoolData.schoolMetadata['XML']['timestamp']
+            data = result.split("\t")
+            title = data[1]
+            type = data[0]
+            id = data[2]
+            agency = data[3]
+            amount = float(data[4])
+            item = PyRSS2Gen.RSSItem(title = title,
+                    link = "#",
+                    description = "%s from the %s in the amount of $%f with id %s" % (type, agency, amount, id),
+                    guid = PyRSS2Gen.Guid(title),
+                    categories = ["DoD", type],
+                    pubDate = datetime.datetime.fromtimestamp(timestamp))
+            items.append(item)
+
+        return items
+
     def PRNewsSearch(self, hostname):
         #whoisStore = whois.WhoisStore()
         #schoolName = whoisStore.getSchoolName(hostname)
@@ -139,6 +232,41 @@ class ProcessBase(object):
         results = u"\n".join(unicode(item, "utf-8") for item in schoolData.getPRNews())
         
         return results
+
+    def PRNewsSearchRSS2(self, hostname):
+        #whoisStore = whois.WhoisStore()
+        #schoolName = whoisStore.getSchoolName(hostname)
+        #schoolData = db.SchoolData(schoolName)
+
+        whoisStore = self.getWhois()
+        schoolName = whoisStore.getSchoolName(hostname)
+        schoolData = self.getSchoolData(schoolName)
+
+        # TODO
+        # Make this less atomic; allow the ability to return smaller chunks, random bits, etc.
+        # This means we need to come up with a REST api, as well as return error messages
+        print "|| MAICgregator server || Getting PR data"
+        #web.header('Content-Encoding', 'utf-8')
+        #results = u"\n".join(unicode(item, "utf-8") for item in schoolData.getPRNews())
+        results = schoolData.getPRNews()
+        items = []
+        for result in results:
+            timestamp = schoolData.schoolMetadata['PRNews']['timestamp']
+            soup = BeautifulSoup(result)
+            title = soup.a.contents[1].strip()
+            description = title
+            url = soup.a['href']
+
+            item = PyRSS2Gen.RSSItem(title = title,
+                    link = url,
+                    description = description,
+                    guid = PyRSS2Gen.Guid(url),
+                    categories = ["PR News"],
+                    author = schoolName,
+                    pubDate = datetime.datetime.fromtimestamp(timestamp))
+            items.append(item)
+
+        return items
 
     def DoDSTTR(self, hostname):
         # Interesting keys to return in our result
@@ -160,8 +288,88 @@ class ProcessBase(object):
         for contract in STTRData:
             output += "\t".join(unicode(contract[key], errors='replace') for key in usefulKeys) + "\n"
         
-        
         return output
+
+    def DoDSTTRRSS2(self, hostname):
+        # Interesting keys to return in our result
+        usefulKeys = ["PK_AWARDS", "AGENCY", "CONTRACT", "AWARD_AMT", "PI_NAME", "FIRM", "URL", "PRO_TITLE", "WholeAbstract"]
+        # TODO
+        # Deal with case when we don't get a school name back
+        #whoisStore = whois.WhoisStore()
+        #schoolName = whoisStore.getSchoolName(hostname)
+        #schoolData = db.SchoolData(schoolName)
+
+        whoisStore = self.getWhois()
+        schoolName = whoisStore.getSchoolName(hostname)
+        schoolData = self.getSchoolData(schoolName)
+
+        print "|| MAICgregator server || Getting STTR RSS data"
+        STTRData = schoolData.getSTTR()
+        
+        items = []
+        for contract in STTRData:
+            timestamp = schoolData.schoolMetadata['STTR']['timestamp']
+            title = contract["PRO_TITLE"]
+            abstract = contract["WholeAbstract"]
+            amount = float(contract["AWARD_AMT"])
+            piName = contract["PI_NAME"]
+            firm = contract["FIRM"]
+            url = contract["URL"]
+            agency = contract["AGENCY"]
+            
+            description = """%f from the %s to %s and %s
+            <br/>
+            <br/>
+            %s""" % (amount, agency, piName, firm, abstract)
+
+            item = PyRSS2Gen.RSSItem(title = title,
+                    link = "http://" + url,
+                    description = description,
+                    guid = PyRSS2Gen.Guid(url),
+                    categories = ["DoD", "STTR"],
+                                     author = schoolName,
+                    pubDate = datetime.datetime.fromtimestamp(timestamp))
+            items.append(item)
+
+        return items
+   
+class RSS(ProcessBase):
+     def GET(self, hostname, params):
+        process = ProcessSingleton.getProcess()
+        whoisStore = process.getWhois()
+        schoolName = whoisStore.getSchoolName(hostname)
+
+        paramsMapping = {'GoogleNewsSearch': 'GoogleNews',
+                    'DoDBR': 'XML',
+                    'TrusteeRelationshipSearch': 'Trustees',
+                    'DoDSTTR': 'STTR',
+                    'PRNewsSearch': 'PRNews'}
+
+        title = "MAICgregator feed for %s and data sources %s" % (schoolName, params)
+        link = "http://maicgregator.org/MAICgregator/feed/rss" + hostname + "/" + params
+        description = "This is an automatically generated RSS feed of information that is pertinent to the military-academic-industrical complex (MAIC) of %s.  Any questions or comments should go to info -at- maicgregator --dot-- org" % schoolName
+
+
+        paramList = params.split("+")
+
+        latestTimestamp = 0
+        items = []
+        for param in paramList:
+            print param
+            actualParam = paramsMapping[param]
+            timestamp = process.getSchoolData(schoolName).schoolMetadata[actualParam]['timestamp']
+            if (timestamp > latestTimestamp):
+                latestTimestamp = timestamp
+            resultFunction = getattr(process, param + "RSS2")
+            results = resultFunction(hostname)
+            items.extend(results)
+        rss = PyRSS2Gen.RSS2(title = title,
+                    link = link,
+                    description = description,
+                    lastBuildDate = datetime.datetime.fromtimestamp(latestTimestamp),
+                    items = items)
+        
+        return rss.to_xml()
 
 class Aggregate(ProcessBase):
 
